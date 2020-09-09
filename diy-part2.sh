6539c56541bc7e9ec0c3250d0edb770dcf3f8380
mazheng127 <mazheng127@gmail.com>
2020-09-10 04:22:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+git clone https://github.com/s1oz/luci-app-koolproxyR.git ./package/luci-app-koolproxyR
